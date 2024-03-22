2d0d8b46f920e811f6d05552c4810ebd650e05d2
Bastien <bastien@MacBook-de-Bastien.local>
2024-03-22 09:44:07 +0100

    author

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,9 +5,16 @@ import { getDetails } from '../validators/index.js'
 
 const router = express.Router()
 
+
 router.get('/api/products', async (req, res, next) => {
-  res.status(600).send()
-})
+  try {
+    const products = await Product.findAll(); // Remplacez cette ligne avec la méthode de votre ORM pour récupérer tous les produits
+    res.status(200).json(products); // Renvoie les produits récupérés sous forme de réponse JSON
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 router.get('/api/products/:productId', async (req, res) => {
   res.status(600).send()

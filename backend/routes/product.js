7062d099a465110df6e4372a7dbadbf91c0b5e8f
Bastien <bastien@MacBook-de-Bastien.local>
2024-03-22 12:24:24 +0100

    début delete

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -46,8 +46,57 @@ router.put('/api/products/:productId', async (req, res) => {
   res.status(600).send()
 })
 
-router.delete('/api/products/:productId', async (req, res) => {
+router.delete('/api/products/:productId', authMiddleware, async (req, res) => {
+
+  try {
+
+    let { productId } = req.params;
+
+    req.body.sellerId = req.user.id;
+
+    let data = await Product.findOne(
+      {
+        where: { id: productId },
+        include: [{
+          model: User,
+          as: 'seller',
+          attributes: ['id', 'username', 'admin']
+        }, {
+          model: Bid,
+          as: 'bids',
+          attributes: ['id', 'price', 'date'],
+          include: [{
+            model: User,
+            as: 'bidder',
+            attributes: ['id', 'username']
+          }]
+        }]
+      }
+    )
+
+    if (!data) {
+      res.status(404).send()
+    } else if (data.sellerId !== req.user.id && !req.user.admin) {
+      res.status(403).send()
+    } else {
+      res.status(204).json(await Product.destroy(
+        { where: { id: productId } }
+      ))
+    }
+
+  } catch (error) {
+    res.status(400).json({ error: "Invalid or missing fields", details: error })
+  }
+
   res.status(600).send()
+  const found = Product.some(todo => todo.id === req.params.id)
+  if (!found) {
+    res.status(400).json({ msg: `No meber whit id of ${req.params.id}` })
+  } else {
+    Product.filter(todo => todo.id !== req.params.id)
+    res.json(Product)
+  }
 })
+
 
 export default router

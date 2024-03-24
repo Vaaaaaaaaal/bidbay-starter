b05be2d02dba69fb8e68fe3acae1ae2eabd0cdb6
iutbgdin <iutbgdin@iutabgdinvm-46.iutbourg.univ-lyon1.fr>
2024-03-24 16:21:59 +0100

    DELETE bids

--- backend/routes/bid.js
+++ backend/routes/bid.js
@@ -5,8 +5,20 @@ import { getDetails } from '../validators/index.js'
 
 const router = express.Router()
 
-router.delete('/api/bids/:bidId', async (req, res) => {
-  res.status(600).send()
+router.delete('/api/bids/:bidId', authMiddleware, async (req, res) => {
+  try {
+    const bidToDestroy = await Bid.findByPk(req.params.bidId)
+    if (!bidToDestroy) {
+      return res.status(404).json({ message: 'Bid non trouvé' })
+    }
+    if (bidToDestroy.bidderId !== req.user.id && !req.user.admin) {
+      return res.status(403).json({ message: 'Action non autorisée' })
+    }
+    await bidToDestroy.destroy()
+    res.status(204).json()
+  } catch (err) {
+    res.status(401).json({ message: err.message })
+  }
 })
 
 router.post('/api/products/:productId/bids', async (req, res) => {
